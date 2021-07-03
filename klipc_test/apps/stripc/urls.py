4c1aaeca7a10a20b135f6aa152cf081441efe362
kongguanghua <204516ab>
2021-07-03 21:21:00 +0800

    test

---
+++ klipc_test/apps/stripc/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from apps.stripc import views
+
+router = DefaultRouter()
+
+urlpatterns = [
+    # 支付请求接口
+    path('create-checkout-session/', views.StripeOrder.as_view()),
+    # 支付回调接口
+    path('stripe_webhooks/', views.StripeCallback.as_view()),
+    # checkout html test url
+    path('checkout/', views.CheckoutClike.as_view()),
+    path('success/', views.Success.as_view()),
+    path('chancel/', views.Cancel.as_view()),
+
+]
